cc98f81a864e0f10355d7f728b8296bbdc7dd9b8
mayur-keswani <mayurkeswani2001@gmail.com>
2024-07-27 18:18:27 +0530

    feat: upload atatchment to firebase bucket

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,8 +9,10 @@ import { onRequest } from "firebase-functions/v1/https";
 import { registerHandler } from "./register";
 import { healthcheckHandler } from "./healthcheck";
 import { loginHandler } from "./login";
+import { uploadHandler } from "./upload";
 
 
 export const healthcheck = onRequest(healthcheckHandler);
 export const register = onRequest(registerHandler);
 export const login = onRequest(loginHandler)
+export const upload = onRequest(uploadHandler)
---
+++ functions/src/upload.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "firebase-functions";
+const admin = require("firebase-admin");
+
+
+export const uploadHandler = async (request: Request, response: Response) => {
+  if (request.method !== "POST") {
+    response.status(405).send("Method Not Allowed");
+  }
+  try {
+    const { base64, filename,filetype } = request.body.input.attachment;
+    const bucket = admin.storage().bucket();
+    const buffer = Buffer.from(base64, "base64");
+    const file = bucket.file(filename);
+    
+    await file.save(buffer, {
+      metadata: {
+        contentType: filetype, // Adjust this based on your image type
+      },
+    });
+     // Make the file public
+     await file.makePublic();
+
+    // Generate the public URL
+    const url = `https://storage.googleapis.com/${bucket.name}/${filename}`;
+    
+    response.status(200).send({
+      filename,
+      filetype,
+      url
+    });
+  } catch (error: any) {
+    console.log({ error });
+    response.status(500).send({ message: `Message: ${error.message}` });
+  }
+};
